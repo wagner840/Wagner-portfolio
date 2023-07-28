cca89c1f83cd5ca5cf33cc81b2ee205b7ccbb2c5
wagner840 <wagnerlevel2@gmail.com>
2023-07-28 00:22:52 -0300

    fix

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
   nameUser: "Wagner Carvalho",
-  githubUser: "wagner840",
+  githubUser: "wagner841",
   linkedinUser: "wagner-guilherme-200a67204",
   whatsappNumber: "+5541995530003",
 };
